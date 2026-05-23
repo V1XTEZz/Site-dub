ddf594c7d4461399547fc4d493ec73afb9cc1f62
V1XTEZz <contact_v1xtezz@proton.me>
2026-05-23 17:55:59 +0300

    Update volumeSync.js

--- volumeSync.js
+++ volumeSync.js
@@ -1,14 +1,22 @@
 var INITIAL_VOLUME = 0.5;
 var audios = document.querySelectorAll("audio");
+var videos = document.querySelectorAll("video");
 
 audios.forEach(function(a) { a.volume = INITIAL_VOLUME; });
+videos.forEach(function(a) { a.volume = INITIAL_VOLUME; });
 
 function syncVolume(value) {
   audios.forEach(function(a) { a.volume = value; });
+  videos.forEach(function(a) { a.volume = value; });
 }
 
 audios.forEach(function(audio) {
   audio.addEventListener("volumechange", function() {
     syncVolume(audio.volume);
+  });
+});
+videos.forEach(function(video) {
+  videos.addEventListener("volumechange", function() {
+    syncVolume(video.volume);
   });
 });
