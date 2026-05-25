5fe906a3ab27f27b398bad9c5fef6ce2a9523fa9
V1XTEZz <contact_v1xtezz@proton.me>
2026-05-25 13:52:54 +0300

    Update textHover.js

--- textHover.js
+++ textHover.js
@@ -4,15 +4,10 @@ const text =
       document.quetySelector(".JATxt");
 
 function hoverOn() {
-    image.style.width = "22%";
+    image.style.width = "23%";
     image.style.filter = "blur(2px) brightness(0.7)";
-    
-    text.style.fontSize = "110%";
 }
-
 function hoverOff() {
     image.style.width = "20%";
     image.style.filter = "blur(0px) brightness(1)";
-    
-    text.style.fontSize = "100%";
 }
